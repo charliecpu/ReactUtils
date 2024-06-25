9d2bd8a98991a41d92414dd73c0267f50902eea6
mordechaigellis <mgellis@cpucodeschool.com>
2024-06-25 14:03:49 +0000

    3-28-3 before redo

--- src/UserStore.ts
+++ src/UserStore.ts
@@ -1,32 +1,51 @@
-import { create } from "zustand";
+import { StoreApi, UseBoundStore, create } from "zustand";
+import { createAPI } from "./DataUtil";
 
 interface User {
     username: string,
-    role: string,
+    rolename: string,
+    rolerank: number,
+    sessionkey: string,
+    errorMessage: string,
     isLoggedIn: boolean,
     login: (username: string, password: string) => Promise<void>,
     logout: () => void,
 }
+
+async function loginUser(apiUrl: string, username: string, password: string): Promise<User> {
+    apiUrl = apiUrl + "user/";
+    const postData = createAPI(apiUrl, "").postData;
+    const user = await postData<User>("login", { username, password });
+    return user;
+}
+
+let userstore: UseBoundStore<StoreApi<User>>;
+
 const keyname = "userstore";
-export const useUserStore = create<User>(
-    (set) => {
-        const storedvalue = sessionStorage.getItem(keyname);
-        const initialvals = storedvalue ?
-            JSON.parse(storedvalue)
-            : { username: "", role: "", isLoggedIn: false, }
-        return {
-            ...initialvals,
-            logout: () => {
-                const newstate = { username: "", role: "", isLoggedIn: false };
-                sessionStorage.setItem(keyname, JSON.stringify(newstate))
-                set(newstate);
-            },
-            login: async (username: string, password: string) => {
-                const roleval = username.toLowerCase().startsWith("x") && password != "" ? "admin" : "user";
-                const newstate = { username: username, role: roleval, isLoggedIn: true };
-                sessionStorage.setItem(keyname, JSON.stringify(newstate));
-                set(newstate);
+export function createUserStore(apiUrl: string) {
+    if (!userstore) {
+        userstore = create<User>(
+            (set) => {
+                const storedvalue = sessionStorage.getItem(keyname);
+                const initialvals = storedvalue ?
+                    JSON.parse(storedvalue)
+                    : { username: "", rolename: "", rolerank: "", sessionkey: "", errorMessage: "", isLoggedIn: false, }
+                return {
+                    ...initialvals,
+                    logout: () => {
+                        const newstate = { username: "", rolename: "", rolerank: 0, sessionkey: "", errorMessage: "", isLoggedIn: false };
+                        sessionStorage.setItem(keyname, JSON.stringify(newstate))
+                        set(newstate);
+                    },
+                    login: async (username: string, password: string) => {
+                        const user = await loginUser(apiUrl, username, password);
+                        const newstate = { username: user.username, rolename: user.rolename, rolerank: user.rolerank, sessionkey: user.sessionkey, errorMessage: user.errorMessage, isLoggedIn: true };
+                        sessionStorage.setItem(keyname, JSON.stringify(newstate));
+                        set(newstate);
+                    }
+                }
             }
-        }
+        )
     }
-);
+    return userstore;
+};

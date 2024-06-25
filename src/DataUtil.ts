d34a8302824d0a15971471d97fc77fb2a19673da
mordechaigellis <mgellis@cpucodeschool.com>
2024-06-25 14:02:12 +0000

    3-28-3 in process

--- src/DataUtil.ts
+++ src/DataUtil.ts
@@ -1,16 +1,21 @@
 import { FieldValues } from "react-hook-form";
 
-export function createAPI(baseurl: string) {
+export function createAPI(baseurl: string, sessionkey: string) {
     async function fetchData<T>(url: string): Promise<T> {
         url = baseurl + url;
-        const r = await fetch(url);
+        const r = await fetch(url, {
+            headers: { "Authorization": `Bearer ${sessionkey}` }
+        });
         const data = await r.json();
         return data;
     }
 
     async function deleteData<T>(url: string): Promise<T> {
         url = baseurl + url;
-        const r = await fetch(url, { method: "DELETE" });
+        const r = await fetch(url, {
+            method: "DELETE",
+            headers: { "Authorization": `Bearer ${sessionkey}` }
+        });
         const data = await r.json();
         return data;
     }
@@ -21,7 +26,8 @@ export function createAPI(baseurl: string) {
             method: "POST",
             body: JSON.stringify(form),
             headers: {
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${sessionkey}`
             }
         });
         const data = await r.json();
